09fd3bb15f24c4ee69d1a92732e9243a1f8fc901
OrElyahu <student44now@gmail.com>
2023-03-28 13:57:38 +0300

    changed waypoint views attribute to be a dictionary with direction and image

--- objects/Waypoint.py
+++ objects/Waypoint.py
@@ -1,12 +1,21 @@
 import pickle
 
 from typing import List
+from enum import Enum
+
+
+class Direction(Enum):
+    UP = 'up'
+    DOWN = 'down'
+    LEFT = 'left'
+    RIGHT = 'right'
 
 
 class Waypoint:
-    def __init__(self, waypoint_id: str = None, neighbor_ids: List[str] = None):
+    def __init__(self, waypoint_id: str = None, neighbors_views=None):
         self._id = waypoint_id
-        self._neighbor_ids = neighbor_ids if neighbor_ids is not None else []
+        self._neighbors_views = neighbors_views if neighbors_views is not None \
+            else {Direction.UP: None, Direction.DOWN: None, Direction.LEFT: None, Direction.RIGHT: None}
 
     def getId(self):
         return self._id
@@ -14,16 +23,20 @@ class Waypoint:
     def setId(self, waypoint_id):
         self._id = waypoint_id
 
-    def get_neighbor_ids(self):
-        return self._neighbor_ids
+    def get_neighbor_views(self):
+        return self._neighbors_views
 
-    def setNeighborIds(self, neighbor_ids):
-        self._neighbor_ids = neighbor_ids if neighbor_ids is not None else []
+    def set_neighbors_views(self, neighbors_ids):
+        self._neighbors_views = neighbors_ids if neighbors_ids is not None else self._neighbors_views
+
+    def set_neighbor_view(self, neighbor_id, direction):
+        if direction in self._neighbors_views.keys():
+            self._neighbors_views[direction] = neighbor_id
 
     def __str__(self):
         return "Waypoint{" + \
                "id='" + str(self._id) + '\'' + \
-               ", neighborIds=" + str(self._neighbor_ids) + \
+               ", neighborIds=" + str(self._neighbors_views) + \
                '}'
 
     # serialization
